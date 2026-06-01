e78e9e7180c0fbc338fd877c2a6fdc3ac4f092e9
oydzhi <bachurinvv54@gmail.com>
2026-06-01 20:25:34 +0500

    Add README

--- obj/Debug/net8.0/LW_10.AssemblyInfo.cs
+++ obj/Debug/net8.0/LW_10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LW_10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64334b4702eede3245a439eb88699ba522e25660")]
 [assembly: System.Reflection.AssemblyProductAttribute("LW_10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LW_10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
